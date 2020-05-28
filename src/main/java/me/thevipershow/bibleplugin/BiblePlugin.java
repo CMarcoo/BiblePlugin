694ad6ae4757424b8758fcf8a16470833a5fdddf
TheVIperShow <thevipershowita@gmail.com>
2020-05-28 22:17:26 +0200

    Project structure

---
+++ src/main/java/me/thevipershow/bibleplugin/BiblePlugin.java
@@ -0,0 +1,12 @@
+package me.thevipershow.bibleplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BiblePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
